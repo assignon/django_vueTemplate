337525c76b970d8864b4593c3275ef222f99a7b7
Yanick <kevineasky@gmail.com>
2020-04-13 00:09:50 +0200

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
-};
+  devServer: {
+    proxy: {
+      '^/': {
+        target: 'http://127.0.0.1:8000/',
+        ws: false,
+      }
+    }
+  },
+  // outputDir must be added to Django's TEMPLATE_DIRS
+  outputDir: './dist/',
+  // assetsDir must match Django's STATIC_URL
+  assetsDir: 'static',
+}
