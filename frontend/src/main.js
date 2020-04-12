720d397e3abb43e7c9bbb807cf2e102b2e26a219
yanick Assignon <yanick.py@localhost.localdomain>
2020-04-13 00:04:09 +0200

    first commit

---
+++ frontend/src/main.js
@@ -0,0 +1,37 @@
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import vuetify from "./plugins/vuetify";
+import VAnimateCss from "v-animate-css";
+import Vuex from "vuex";
+import Axios from "axios";
+//Aos imports
+import AOS from "aos";
+import "aos/dist/aos.css";
+// vue session
+// import VueSession from 'vue-session'
+
+Vue.config.productionTip = false;
+Vue.use(VAnimateCss);
+Vue.use(Vuex);
+// Vue.use(VueSession,{persist: true})
+
+Axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+Axios.defaults.xsrfCookieName = "XCSRF-TOKEN";
+Axios.defaults.withCredentials = true;
+
+Vue.prototype.$axios = Axios;
+
+new Vue({
+  created() {
+    AOS.init();
+  },
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
---
+++ portfolio/router.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+# from account.views import Account_view
+
+
+router = routers.DefaultRouter()
+# router.register('product', Product_view, base_name='productdata')
